52f07f8aed3f5e58cb32663ed49b45b5dfe10d6b
Alexis de Lattre <alexis.delattre@akretion.com>
2020-03-02 16:14:21 +0100

    [MIG] account_invoice_transmit_method to v13

--- account_invoice_transmit_method/models/account_move.py
+++ account_invoice_transmit_method/models/account_move.py
@@ -1,45 +1,44 @@
-# Copyright 2017-2022 Akretion France (http://www.akretion.com)
+# Copyright 2017-2020 Akretion France (http://www.akretion.com)
 # @author: Alexis de Lattre <alexis.delattre@akretion.com>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
 
 
 class AccountMove(models.Model):
-    _inherit = "account.move"
+    _inherit = 'account.move'
 
     transmit_method_id = fields.Many2one(
-        "transmit.method",
-        string="Transmission Method",
-        compute="_compute_transmit_method_id",
-        store=True,
-        readonly=False,
-        tracking=True,
-        ondelete="restrict",
-        domain="""
-            [
-                '|',
-                ('customer_ok', '=', invoice_filter_type_domain in (False, 'sale')),
-                ('supplier_ok', '=', invoice_filter_type_domain in (False, 'purchase')),
-            ]
-        """,
-    )
+        'transmit.method', string='Transmission Method',
+        tracking=True, ondelete='restrict')  # domain in the view
+    # Field used to match specific invoice transmit method
+    # to show/display fields/buttons, add constraints, etc...
     transmit_method_code = fields.Char(
-        string="Transmission Method Code",
-        related="transmit_method_id.code",
-        help="Technical field used for UX purposes",
-    )
+        related='transmit_method_id.code', store=True)
 
-    @api.depends("partner_id", "move_type")
-    def _compute_transmit_method_id(self):
-        for rec in self:
-            if rec.partner_id and rec.is_sale_document():
-                rec.transmit_method_id = (
-                    rec.partner_id.customer_invoice_transmit_method_id
-                )
-            elif rec.partner_id and rec.is_purchase_document():
-                rec.transmit_method_id = (
-                    rec.partner_id.supplier_invoice_transmit_method_id
-                )
+    @api.onchange('partner_id', 'company_id')
+    def _transmit_method_partner_change(self):
+        if self.partner_id and self.type:
+            if self.type in ('out_invoice', 'out_refund'):
+                self.transmit_method_id = self.partner_id.\
+                    customer_invoice_transmit_method_id.id or False
             else:
-                rec.transmit_method_id = False
+                self.transmit_method_id = self.partner_id.\
+                    supplier_invoice_transmit_method_id.id or False
+        else:
+            self.transmit_method_id = False
+
+    @api.model
+    def create(self, vals):
+        if (
+                'transmit_method_id' not in vals and
+                vals.get('type') and
+                vals.get('partner_id')):
+            partner = self.env['res.partner'].browse(vals['partner_id'])
+            if vals['type'] in ('out_invoice', 'out_refund'):
+                vals['transmit_method_id'] =\
+                    partner.customer_invoice_transmit_method_id.id or False
+            else:
+                vals['transmit_method_id'] =\
+                    partner.supplier_invoice_transmit_method_id.id or False
+        return super().create(vals)
