9c2c525450d7254ed9bb1861a1ad65a3a7dd2a2c
Sergio Teruel <sergio.teruel@tecnativa.com>
2020-07-14 08:45:11 +0200

    [FIX] account_invoice_refund_link: Fix foreignkey exception when a credit note invoice has done from sale order.

--- account_invoice_refund_link/wizards/account_move_reversal.py
+++ account_invoice_refund_link/wizards/account_move_reversal.py
@@ -7,11 +7,11 @@ from odoo import models
 class AccountMoveReversal(models.TransientModel):
     _inherit = "account.move.reversal"
 
-    def reverse_moves(self, is_modify=False):
+    def reverse_moves(self):
         """
         Only link invoice lines with theirs original lines when the reversal
         move has been done from reversal wizard.
         """
         return super(
             AccountMoveReversal, self.with_context(link_origin_line=True)
-        ).reverse_moves(is_modify=is_modify)
+        ).reverse_moves()
