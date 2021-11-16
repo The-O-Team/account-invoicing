4bd0fd0d7ce983a0e61eb24341bc4c6d986f9ed7
François Honoré <francois.honore@acsone.eu>
2021-11-16 09:29:21 +0100

    [14.0][IMP] acount_invoice_refund_link: add index

--- account_invoice_refund_link/models/account_move_line.py
+++ account_invoice_refund_link/models/account_move_line.py
@@ -1,6 +1,6 @@
 # Copyright 2004-2011 Pexego Sistemas Informáticos. (http://pexego.es)
-# Copyright 2016 Tecnativa - Antonio Espinosa
-# Copyright 2014-2023 Tecnativa - Pedro M. Baeza
+# Copyright 2016 Antonio Espinosa <antonio.espinosa@tecnativa.com>
+# Copyright 2014-2018 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo import fields, models
@@ -12,7 +12,8 @@ class AccountInvoiceLine(models.Model):
     origin_line_id = fields.Many2one(
         comodel_name="account.move.line",
         string="Original invoice line",
-        help="Original invoice line to which this refund invoice line is referred to",
+        help="Original invoice line to which this refund invoice line "
+        "is referred to",
         copy=False,
         index=True,
     )
@@ -23,13 +24,3 @@ class AccountInvoiceLine(models.Model):
         help="Refund invoice lines created from this invoice line",
         copy=False,
     )
-
-    def copy_data(self, default=None):
-        """Link refund lines with the original ones when copying move lines from the
-        `_reverse_move_vals` method.
-        """
-        res = super().copy_data(default=default)
-        if self.env.context.get("link_origin_line"):
-            for line, values in zip(self, res, strict=False):
-                values["origin_line_id"] = line.id
-        return res
