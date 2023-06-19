42b33b741a834c1243127d4b4e39ac09e25a92f5
Pieter Paulussen <pieterpaulussen@code-source.be>
2023-06-19 16:46:28 +0200

    [MIG] account_invoice_fixed_discount: Migration to 16.0

--- account_invoice_fixed_discount/models/account_tax.py
+++ account_invoice_fixed_discount/models/account_tax.py
@@ -26,9 +26,7 @@ class AccountTax(models.Model):
         handle_price_include=True,
         extra_context=None,
     ):
-        """Insert the un-rounded discount value in the resulting tax computation
-        values.
-        """
+        """Insert the un-rounded discount value in the resulting tax computation values."""
         res = super()._convert_to_tax_base_line_dict(
             base_line,
             partner=partner,
@@ -46,10 +44,6 @@ class AccountTax(models.Model):
             handle_price_include=handle_price_include,
             extra_context=extra_context,
         )
-        if (
-            base_line
-            and base_line._name == "account.move.line"
-            and base_line.discount_fixed
-        ):
+        if base_line._name == "account.move.line" and base_line.discount_fixed:
             res["discount"] = base_line._get_discount_from_fixed_discount()
         return res
